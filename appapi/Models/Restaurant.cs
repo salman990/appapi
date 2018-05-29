8294bd8fb1bd5439835d37261c77766c7233ac07
salman PC <salmankhan990@hotmail.com>
2018-05-29 16:42:15 +0200

    start

---
+++ appapi/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appapi.Models
+{
+    public class Restaurant
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
